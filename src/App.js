a7402e04a12ef055fce101c3a043eaaca77f093e
Nelson Palacios <nrpc10@gmail.com>
2023-10-03 20:31:36 -0500

    create a h1 headind and render the list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React from 'react';
 
+const todoList = [
+  {
+    id: 1,
+    title: "Read the book Road to React",
+  },
+  {
+    id: 2,
+    title: "See videos from Learn the App",
+  },
+  {
+    id: 3,
+    title: "Submit my assigment",
+  },
+  {
+    id: 4,
+    title: "Practice JavaScript",
+  },
+];
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div>
       <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <h1>Todo List</h1>
       </header>
+        <ul>
+          {todoList.map(function (item) {
+            return <li>{item.id} {item.title}</li>;
+            })}
+        </ul>
     </div>
   );
-}
+};
 
 export default App;
